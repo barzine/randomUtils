470d9c713dc9377afccf9d44283ae4f1f22efe9c
M.P. Barzine <barzine@gmail.com>
2014-06-16 18:11:04 +0100

    Create wstrip.pl
    because installing perl modules on the cluster is a pain

---
+++ wstrip.pl
@@ -0,0 +1,31 @@
+#!/usr/local/bin/perl
+
+#define function
+sub strip {
+        my$string= $_[0];
+        $string =~ s/^\s+|\s+$//g ;
+        return($string);
+}
+
+my$numArgs = $#ARGV + 1;
+if($numArgs==0){
+        print "\nUsage lstrip.pl file1 [file2 .. fileN]\n\n bye bye\n\n";
+        exit;
+}
+foreach $argnum (0 .. $#ARGV) {
+        if( -e $ARGV[$argnum]){
+                print "stripping left spaces of $ARGV[$argnum]:";
+                open(TOCLEAN,$ARGV[$argnum]) or die "cannot open file!";
+                open(my $new,'>',"$ARGV[$argnum]_cleaned") or die "Cannot create new file!";
+                while(my$line=<TOCLEAN>){
+                        my$string=strip($line);
+                        print $new "$string\n";
+                }
+                close(TOCLEAN);
+                close(NEW);
+                print " done\n\n";
+        }else{
+                print "file: $ARGV[$argnum] not found\n";
+        }
+}
+exit;
